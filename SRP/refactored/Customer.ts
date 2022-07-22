8cea6de945dc7488219b90da55f393103575c93e
iftakharalamrizve <iftakharalam1100@gmail.com>
2022-07-22 17:19:58 +0600

    violation code remove srp

--- SRP/refactored/Customer.ts
+++ SRP/refactored/Customer.ts
@@ -11,30 +11,5 @@ class Customer{
         this.name = name;
         this.age = age;
     }
-    
-    public calculatePrice(tax: number):number
-    {
-        let totalItemSize: number = this.items.length;
-        let itemPrice: number = 0 ;
-        for (let item of this.items){
-            itemPrice += item.getPrice()
-        }
-
-        if(totalItemSize > 2){
-            this.plasticBagPrice  = this.numberOfBag * 0.5;
-            console.log("More than 2 items purchased. Reducing plastic bag price");
-        }
-
-        if(itemPrice <= 100)
-        {
-            tax = tax * .80;
-            console.log("Item price is more than 100. 80 Percent of the actual tax applicable");
-        }else if(itemPrice >= 100 && itemPrice <= 150){
-            console.log("Item price is more than 100 but less than 150. 90 Percent of the actual tax applicable");
-            tax = tax * .90
-        }
-        let totalPrice: number = itemPrice * tax + this.numberOfBag * this.plasticBagPrice;
-        return totalPrice;
-    }
 }
 export default Customer;
